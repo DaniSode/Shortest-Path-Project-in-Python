15dc2760bfad2c354191b9d7f920d09058a4fdf1
DaniSode <danisode@student.chalmers.se>
2022-02-02 09:19:20 +0100

    add menu

--- Hej.py
+++ Hej.py
@@ -6,68 +6,125 @@ from scipy.sparse.csgraph import shortest_path
 from scipy.spatial import cKDTree
 import time
 
-
+##### WHICH FILENAME #####
 def which_file(filename):
+
+    # A function taking the name of the file as input
+    # and returns the corresponding radius, start city and end city.
+
     radius = start_node = end_node = None
+
     if filename == 'SampleCoordinates':
         radius = 0.08
         start_node = 0
         end_node = 5
+
     elif filename == 'HungaryCities':
         radius = 0.005
         start_node = 311
         end_node = 702
+
     elif filename == 'GermanyCities':
         radius = 0.0025
         start_node = 1573
         end_node = 10584
+
     return radius, start_node, end_node
 
 
+##### TASK 1 #####
 def read_coordinate_file(filename):
+
+    # A function taking the specific file as input and returns a list of all the coordinates
+    # of every available city as coord_list. The function reads line by line with a while loop,
+    # in the loop the line is modified ending up as two float numbers describing longitude
+    # and latitude of the cities. They are appended to a list which in turn are used to calculate
+    # the final x and y coordinate of each city.
+
     with open(filename + ".txt", mode='r') as file:
         sample_coord = file.readline()
         a = []
+
         while sample_coord:
             # Do we need for loop here
             res = [float(line) for line in sample_coord.strip('{}\n').split(',')]
             a.append(res)
             sample_coord = file.readline()
+
     b = np.array(a)
     R = 1
     x = R * np.pi / 180 * b[:, 1]
     y = R * np.log(np.tan(np.pi / 4 + np.pi / 360 * b[:, 0]))
     coord_list = np.array([x, y]).T
+
     return coord_list
 
 
+##### TASK 2, 5 and 7 #####
 def plot_points(coord_list, indices, path):
 
-    # PLOT CITIES
+    # A function taking inputs as coord_list (returned from task 1), indices (defined in the
+    # functions construct_graph_connections or construct_fast_graph_connections) and
+    # path (defined in function construct_graph). The function returns a merged plot of all
+    # sub-tasks
+
+    ##### Task 2 #####
+
+    # The cities are ploted with the scatter command and taking the coordinates of each
+    # city as input showing all cities as dots.
+
     plt.scatter(coord_list[:, 0], coord_list[:, 1], s=5, c="r")
     plt.gca().set_aspect('equal')
 
-    # PLOT AVAILABLE CONNECTIONS
+    ##### TASK 5 #####
+
+    # The available connections between the cities were ploted using a for loop giving the
+    # connected cities the right coordinates using coord_list (all coordinates) and
+    # indices (all possible connections). The coordinates of each connection were appended
+    # to a list and the list were converted to a LineCollection which in turn were ploted
+    # showing all possible connections as grey lines.
+
     city_pair_coord = []
+
     for i, j in indices:
         city_pair_coord.append([[coord_list[i, 0], coord_list[i, 1]], [coord_list[j, 0], coord_list[j, 1]]])
+
     lc = LineCollection(city_pair_coord, linewidth=0.4, colors="gray")
     fig = plt.subplot()
     fig.add_collection(lc)
 
-    # PLOT SHORTEST PATH
+    ##### TASK 7 #####
+
+    # With path (number of cities describing the shortest path between start and end city)
+    # the corresponding coordinates were linked to each city in path using a for loop. The
+    # coordinates were then ploted showing the shortest path as a blue line.
+
     path_coord_x = []
     path_coord_y = []
+
     for city in path:
         path_coord_x.append(coord_list[city, 0])
         path_coord_y.append(coord_list[city, 1])
+
     plt.plot(path_coord_x, path_coord_y, linewidth=1, c="blue")
     plt.show()
 
 
+##### TASK 3 #####
 def construct_graph_connections(coord_list, radius):
+
+    # A function taking coord_list (coordinates of all cities) and radius (the maximum possible
+    # distance between two cities) and returns indices (all possible connections between cities)
+    # and distances (the corresponding distances between the cities). The function are using a
+    # nested for loop firstly to with enumerate give all cities a specific number than by look
+    # at city to city and with the inner loop check against all other connections to see if the
+    # distance is less than the given maximum radius. All possible connections are saved to
+    # indices and distances. By using 'i+1:' in the calculations of tot_distances the loop just
+    # consider all possible connections forward on which exclude duplicates like 1-2 and 2-1.
+
     pair_indices = []
     distances = []
+
     for i, city in enumerate(coord_list):
         dxdy = coord_list - city
         tot_distances = np.sqrt(np.square(dxdy[i + 1:, 0]) + np.square(dxdy[i + 1:, 1]))
@@ -75,24 +132,38 @@ def construct_graph_connections(coord_list, radius):
             if distance <= radius:
                 pair_indices.append([i, i + 1 + j])
                 distances.append(distance)
+
     return np.array(pair_indices), np.array(distances)
 
 
+##### TASK 4 #####
 def construct_graph(indices, distance, N):
+
+    # A function taking indices (possible connections), distances (corresponding distances)
+    # and N (total amount of cities) and returning graph (sparse matrix of indices and distances)
+
     graph = csr_matrix((distance, indices.T), shape=(N, N))
+
     return graph
 
 
+##### TASK 6 #####
 def find_shortest_path(graph, start_node, end_node):
+
+    #
+
     distance, predecessor = shortest_path(graph, directed=False, return_predecessors=True, indices=start_node)
     start_end_dist = distance[end_node]
     a = end_node
     path = [end_node]
+
     while predecessor[a] != start_node:
         a = predecessor[a]
         path.append(a)
+
     path.append(start_node)
     path.reverse()
+
     return path, start_end_dist
 
 
